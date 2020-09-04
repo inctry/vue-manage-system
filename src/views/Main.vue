c5388aa8b74e2db40d2d5a3876535fb157fe85f1
Inctry <inctry@outlook.com>
2020-09-04 16:31:00 +0800

    finish CommonAside.vue

---
+++ src/views/Main.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-container style="height: 100%">
+        <el-aside width="200px">
+            
+            <common-aside></common-aside>
+
+        </el-aside>
+
+        <el-container>
+            
+            <common-header></common-header>
+                
+            <el-main></el-main>
+
+        </el-container>
+    </el-container>    
+</template>
+
+<script>
+import CommonHeader from '../components/CommonHeader.vue'
+import CommonAside from '../components/CommonAside.vue'
+export default {
+    components: {
+        CommonHeader,CommonAside
+    }
+}
+</script>
+
+<style>
+
+</style>
